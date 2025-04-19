bf1cb37deb620dbef9411710ea48addf8dbd7b7f
jerry-1211 <kcjerim1234567@gmail.com>
2025-04-19 19:40:52 +0900

    [step0] Chore: RabbitMQConfig 내용 추가 - Acknowledge Mode 설정

--- src/main/java/com/example/messagequeue/step1/RabbitMQConfig.java
+++ src/main/java/com/example/messagequeue/step1/RabbitMQConfig.java
@@ -1,5 +1,6 @@
 package com.example.messagequeue.step1;
 
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -13,11 +14,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String QUEUE_NAME = "messageQueueForPractice";
+    public static final String QUEUE_NAME = "WorkQueue";
 
     @Bean
     public Queue queue(){
-        return new Queue(QUEUE_NAME, false);
+        return new Queue(QUEUE_NAME, true);
     }
 
     @Bean
@@ -32,6 +33,7 @@ public class RabbitMQConfig {
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(QUEUE_NAME);
         container.setMessageListener(listenerAdapter);
+        container.setAcknowledgeMode(AcknowledgeMode.AUTO);
         return container;
     }
 
