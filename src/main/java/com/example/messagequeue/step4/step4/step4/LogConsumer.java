6c9b26d04d89b4b4e71d2df8d80e79b726e35857
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-22 23:48:14 +0900

    [step4] Feat: LogConsumer 코드 작성 - Rabbit Listener로 설정된 queue에서 메시지를 가져옴

---
+++ src/main/java/com/example/messagequeue/step4/step4/step4/LogConsumer.java
@@ -0,0 +1,24 @@
+package com.example.messagequeue.step4.step4.step4;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class LogConsumer {
+
+    @RabbitListener(queues = RabbitMQConfig.ERROR_QUEUE)
+    public void consumerError(String message){
+        System.out.println("[Error]를 받음 : " + message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.WARN_QUEUE)
+    public void consumerWarn(String message){
+        System.out.println("[Warn]를 받음 : " + message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.INFO_QUEUE)
+    public void consumerInfo(String message){
+        System.out.println("[Info]를 받음 : " + message);
+    }
+}
