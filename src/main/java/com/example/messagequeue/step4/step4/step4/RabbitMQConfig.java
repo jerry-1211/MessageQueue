5aed426d4b0965d6b7f1b5d1c05bbbbeac0e65ae
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-22 23:44:10 +0900

    [step4] Chore: RabbitMQ Config(설정) 세팅 - DirectExchage 사용

---
+++ src/main/java/com/example/messagequeue/step4/step4/step4/RabbitMQConfig.java
@@ -0,0 +1,50 @@
+package com.example.messagequeue.step4.step4.step4;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ERROR_QUEUE = "error_queue";
+    public static final String WARN_QUEUE = "warn_queue";
+    public static final String INFO_QUEUE = "info_queue";
+
+    public static final String DIRECT_EXCHANGE = "direct_exchange";
+
+    @Bean
+    public Queue errorQueue() {
+        return new Queue(WARN_QUEUE, false);
+    }
+
+    @Bean
+    public Queue warnQueue() {
+        return new Queue(ERROR_QUEUE, false);
+    }
+
+    @Bean
+    public Queue infoQueue() {
+        return new Queue(INFO_QUEUE, false);
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(DIRECT_EXCHANGE);
+    }
+
+    @Bean
+    public Binding errorBinding(Queue errorQueue, DirectExchange fanoutExchange) {
+        return BindingBuilder.bind(errorQueue).to(directExchange()).with("error");
+    }
+
+    @Bean
+    public Binding warnBinding(Queue warnQueue, DirectExchange fanoutExchange) {
+        return BindingBuilder.bind(warnQueue).to(directExchange()).with("warn");
+    }
+
+    @Bean
+    public Binding infoBinding(Queue infoQueue, DirectExchange fanoutExchange) {
+        return BindingBuilder.bind(infoQueue).to(directExchange()).with("info");
+    }
+
+}
