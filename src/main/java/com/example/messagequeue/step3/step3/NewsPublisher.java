403c630143b02a19194dee6b42b10c91f59ee478
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-22 19:23:16 +0900

    [step3] Feat: NewsPublisher 코드 작성 - RabbitMQ(브로커)에게 전달하기 위한 계층

---
+++ src/main/java/com/example/messagequeue/step3/step3/NewsPublisher.java
@@ -0,0 +1,29 @@
+package com.example.messagequeue.step3.step3;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewsPublisher {
+    private final RabbitTemplate rabbitTemplate;
+
+    public NewsPublisher(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    private String publishMessage(String news, String messageSuffix){
+        String message = news + messageSuffix;
+        rabbitTemplate.convertAndSend(RabbitMQConfig.FANOUT_EXCHANGE_FOR_NEWS, news, message);
+        return message;
+    }
+
+
+    public String publish(String news){
+        return publishMessage(news, " 관련 새 소식이 있어요 !");
+    }
+
+    public String publishAPI(String news){
+        return publishMessage(news, "- 관련 새 소식이 나왔습니다. (API)");
+    }
+}
+
