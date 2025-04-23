6bddd4348e61f94803350845a3cf937b3766b85e
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-23 23:39:05 +0900

    [step6] Feat: OrderProducer 코드 작성 - 기존 Producer와 크게 다르지 않음

---
+++ src/main/java/com/example/messagequeue/step6/step6/OrderProducer.java
@@ -0,0 +1,21 @@
+package com.example.messagequeue.step6.step6;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendShipping(String message){
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.ORDER_EXCHANGE,
+                "order.completed.shipping",
+                message
+        );
+        System.out.println("[주문 완료. 배송 지시 메시지 생성: " + message + "]");
+    }
+}
