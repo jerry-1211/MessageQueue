b712245e21b901ba1077a66f0e2a3107cb8b142e
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-24 20:44:59 +0900

    [step7] Feat: OrderController, Producer 코드 작성 - step6 코드와 거의 비슷 (변수명만 바뀜)

---
+++ src/main/java/com/example/messagequeue/step7/step7/OrderController.java
@@ -0,0 +1,31 @@
+package com.example.messagequeue.step7.step7;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/step7/api/order")
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    /*
+    * PostMan으로 테스트
+    * http://localhost:8080/step7/api/order?message=success
+    * http://localhost:8080/step7/api/order?message=fail
+    * */
+
+    @GetMapping
+    public ResponseEntity<String> sendOrderMessage(@RequestParam String message){
+        orderProducer.sendShipping(message);
+        return ResponseEntity.ok("Order Completed Message sent: " + message);
+    }
+}
+
