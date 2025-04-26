9a3a9da8c7510d20492d895be60054bc151b250c
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-26 22:54:34 +0900

    [step9] Feat: Message Producer 코드 작성

---
+++ src/main/java/com/example/messagequeue/step9/step9/MessageProducer.java
@@ -0,0 +1,56 @@
+package com.example.messagequeue.step9.step9;
+
+import jakarta.transaction.Transactional;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MessageProducer {
+    private final StockRepository stockRepository;
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageProducer(StockRepository stockRepository, RabbitTemplate rabbitTemplate) {
+        this.stockRepository = stockRepository;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Transactional
+    public void sendMessage(StockEntity stockEntity, String testCase) {
+        rabbitTemplate.execute(channel -> {
+            try {
+                channel.txSelect();
+                stockEntity.setProcessed(false);
+                stockEntity.setCreatedAt(LocalDateTime.now());
+                StockEntity stockEntitySaved = stockRepository.save(stockEntity);
+
+                System.out.println("Stock Saved : " + stockEntitySaved);
+
+                rabbitTemplate.convertAndSend("transactionQueue", stockEntitySaved);
+
+                if ("fail".equalsIgnoreCase(testCase)) {
+                    throw new RuntimeException("트랜잭션 작업중 에러 발생");
+                }
+                channel.txCommit();
+                System.out.println("트랜잭션이 정상적으로 처리 되었음!~");
+            } catch (Exception e) {
+                System.out.println("트랜잭션 실패 : " + e.getMessage());
+                channel.txRollback();
+                throw new RuntimeException("트랜잭션 롤백 완료 ", e);
+            } finally {
+                if (channel != null) {
+                    try {
+                        channel.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        });
+    }
+}
+
+
+
