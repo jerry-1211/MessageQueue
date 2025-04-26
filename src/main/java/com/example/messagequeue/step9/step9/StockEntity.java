e772f33d8a792cf437e00beb60daef2fe0d6db32
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-26 22:52:46 +0900

    [step9] Feat: Entity,Repository 코드 작성

---
+++ src/main/java/com/example/messagequeue/step9/step9/StockEntity.java
@@ -0,0 +1,25 @@
+package com.example.messagequeue.step9.step9;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@ToString
+public class StockEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private int stock;
+
+    private boolean processed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/example/messagequeue/step9/step9/StockRepository.java
@@ -0,0 +1,13 @@
+package com.example.messagequeue.step9.step9;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 기본 CRUD 자동 구현
+ * 메서드 이름 기반의 쿼리 생성
+ * 복잡한 쿼리 지원
+ * 코드를 거의 안쓰고 DB 조작 가능
+ **/
+
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
+}
