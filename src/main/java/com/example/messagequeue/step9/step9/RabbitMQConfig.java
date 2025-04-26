cfb0b98e4e0215f1257b6a024c3fb754e2856e68
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-04-26 22:53:21 +0900

    [step9] Chore: RabbitMQ Config 세팅

---
+++ src/main/java/com/example/messagequeue/step9/step9/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.example.messagequeue.step9.step9;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queue(){
+        return new Queue("transactionQueue", true);
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setChannelTransacted(true);
+        return rabbitTemplate;
+    }
+}
