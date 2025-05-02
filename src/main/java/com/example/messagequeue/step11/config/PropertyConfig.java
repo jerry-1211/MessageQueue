7075559829ca016b2642cc13de1e17990d558604
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-05-02 22:10:12 +0900

    [step11] Chore: 환경 변수 스프링 컨테이너에 추가

---
+++ src/main/java/com/example/messagequeue/step11/config/PropertyConfig.java
@@ -0,0 +1,9 @@
+package com.example.messagequeue.step11.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:properties/env.properties")
+public class PropertyConfig {
+}
